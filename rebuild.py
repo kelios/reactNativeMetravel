5b28587ffea77428caf19f12501ccfbe651d3683
kelios_ <kelios@inbox.ru>
2025-04-01 19:32:00 +0200

    dist prod

--- rebuild.py
+++ rebuild.py
@@ -4,4 +4,6 @@ subprocess.run('yarn cache clean', shell=True)
 subprocess.run('rm -rf node_modules/ package-lock.json yarn.lock', shell=True)
 subprocess.run('rm -rf dist/', shell=True)
 subprocess.run('yarn install', shell=True)
-subprocess.run('EXPO_NO_METRO_LAZY=true yarn expo export -c -p web', shell=True)
+subprocess.run('EXPO_NO_METRO_LAZY=true EXPO_ENV=dev yarn expo export --output-dir dist/dev -c -p web', shell=True)
+subprocess.run('EXPO_NO_METRO_LAZY=true EXPO_ENV=prod yarn expo export --output-dir dist/prod -c -p web', shell=True)
+
